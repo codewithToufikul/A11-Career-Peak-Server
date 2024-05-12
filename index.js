351c2f8ab269565691aa2a492009d53a39b7abac
Toufikul Islam <142156125+imran8990@users.noreply.github.com>
2024-05-13 01:20:06 +0600

    get all jobs data from mongodb

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
+const corsOptions = {
+  origin: ['http://localhost:5173/', 'http://localhost:5173/'],
+  Credential: true,
+  optionSuccessStatus: 200,
+}
 // middleware
 app.use(cors());
 app.use(express.json());
 
 
+var uri = `mongodb://${process.env.DB_USERS}:${process.env.DB_PASSWORD}@ac-jsoylff-shard-00-00.ivo4yuq.mongodb.net:27017,ac-jsoylff-shard-00-01.ivo4yuq.mongodb.net:27017,ac-jsoylff-shard-00-02.ivo4yuq.mongodb.net:27017/?ssl=true&replicaSet=atlas-13wtm6-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ivo4yuq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
 
 
 const client = new MongoClient(uri, {
@@ -26,6 +34,20 @@ async function run() {
   try {
     const jobCollection = client.db("jobDB").collection("jobCollection");
 
+    app.get("/", (req, res)=>{
+      res.send('hello from career peak server')
+    })
+    
+    app.get("/jobs", async(req, res)=>{
+      const result = await jobCollection.find().toArray();
+      res.send(result)
+    })
+    app.get("/jobs/:id", async(req, res)=>{
+      const id = req.params.id;
+      const findId = {_id: new ObjectId(id)};
+      const result = await jobCollection.findOne(findId);
+      res.send(result);
+    })
 
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
